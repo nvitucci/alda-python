7d48f2e66cd834baa733a4962e5673a7d5f2884a
nvitucci <nicola.vitucci@gmail.com>
2022-09-20 12:45:59 +0100

    Add Bencode write tests

--- tests/unit/test_bencode.py
+++ tests/unit/test_bencode.py
@@ -11,11 +11,26 @@ VALUES = [
     ("le", []),
     ("l4:spam4:eggse", [b"spam", b"eggs"]),
     ("l4:spam4:eggsd3:cow3:mooee", [b"spam", b"eggs", {b"cow": b"moo"}]),
+    ("de", {}),
     ("d3:cow3:moo4:spam4:eggse", {b"cow": b"moo", b"spam": b"eggs"}),
     ("d9:publisher3:bob17:publisher-webpage15:www.example.com18:publisher.location4:homee",
         {b"publisher": b"bob", b"publisher-webpage": b"www.example.com", b"publisher.location": b"home"}),
-    ("d3:cow3:moo4:spaml1:a1:beee", {b"cow": b"moo", b"spam": [b"a", b"b"]}),
-    ("d3:cow3:moo4:spamd1:a1:beee", {b"cow": b"moo", b"spam": {b"a": b"b"}}),
+    ("d3:cow3:moo4:spaml1:a1:bee", {b"cow": b"moo", b"spam": [b"a", b"b"]}),
+]
+
+# TODO: there should be no difference with VALUES
+VALUES_NO_BIN = [
+    ("0:", ""),
+    ("4:spam", "spam"),
+    ("i3e", 3),
+    ("le", []),
+    ("l4:spam4:eggse", ["spam", "eggs"]),
+    ("l4:spam4:eggsd3:cow3:mooee", ["spam", "eggs", {"cow": "moo"}]),
+    ("d3:cow3:moo4:spam4:eggse", {"cow": "moo", "spam": "eggs"}),
+    ("d9:publisher3:bob17:publisher-webpage15:www.example.com18:publisher.location4:homee",
+        {"publisher": "bob", "publisher-webpage": "www.example.com", "publisher.location": "home"}),
+    ("d3:cow3:moo4:spaml1:a1:bee", {"cow": "moo", "spam": ["a", "b"]}),
+    ("d3:cow3:moo4:spamd1:a1:bee", {"cow": "moo", "spam": {"a": "b"}}),
 ]
 
 
@@ -31,3 +46,14 @@ class TestBencodeRead:
 
         with pytest.raises(AssertionError):
             Bencode(BytesIO(value.encode()))
+
+
+class TestBencodeWrite:
+    @pytest.mark.parametrize("encoded, decoded", VALUES_NO_BIN)
+    def test_values(self, encoded, decoded):
+        buffer = BytesIO()
+        b = Bencode(buffer)
+        b.write(decoded)
+
+        buffer.seek(0)
+        assert buffer.read().decode() == encoded

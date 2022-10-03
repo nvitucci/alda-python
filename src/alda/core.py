7ee6c782fd0a8c39ab45ec415c2f1954f4822443
nvitucci <nicola.vitucci@gmail.com>
2022-10-04 00:19:18 +0100

    Add Chord with tests

--- src/alda/core.py
+++ src/alda/core.py
@@ -1,5 +1,7 @@
-from typing import List
+from typing import List, Tuple
 
+from . import interval
+from .interval import Interval
 from .pitch import Pitch
 
 
@@ -28,8 +30,11 @@ class Part:
 
 
 class Duration:
-    def __init__(self, length):
+    def __init__(self, length: int):
         self.length = length
+
+    def __eq__(self, other):
+        return self.length == other.length
 
     def __str__(self):
         return str(self.length)
@@ -41,6 +46,9 @@ class Note:
         self.duration = duration
         self.octave = octave
 
+    def __eq__(self, other):
+        return self.pitch == other.pitch and self.duration == other.duration and self.octave == other.octave
+
     def serialize(self, with_octave: bool = False) -> str:
         ser = f"{self.pitch}{self.duration}"
 
@@ -48,3 +56,50 @@ class Note:
             ser = f"o{self.octave} {ser}"
 
         return ser
+
+
+DOM_7 = (interval.M3, interval.P5, interval.m7)
+MAJ_7 = (interval.M3, interval.P5, interval.M7)
+MIN_MAJ_7 = (interval.m3, interval.P5, interval.M7)
+MIN_7 = (interval.m3, interval.P5, interval.m7)
+AUG_MAJ_7 = (interval.M3, interval.A5, interval.M7)
+AUG_7 = (interval.M3, interval.A5, interval.m7)
+MIN_7_DIM_5 = (interval.m3, interval.d5, interval.m7)
+DIM_7 = (interval.m3, interval.d5, interval.d7)
+DOM_7_DIM_5 = (interval.M3, interval.d5, interval.m7)
+
+
+class Chord:
+    def __init__(self, notes: List[Note]):
+        self.notes = notes
+
+    def __eq__(self, other):
+        return self.notes == other.notes
+
+    def __repr__(self):
+        return self.serialize()
+
+    @staticmethod
+    def build_chord(base_note: Note, name: Tuple[Interval], inversion_base: Pitch = None):
+        pitches = [base_note.pitch] + [base_note.pitch.get_interval(part) for part in name]
+        if inversion_base is not None:
+            try:
+                inv_index = pitches.index(inversion_base)
+            except ValueError:
+                raise ValueError(f"'{inversion_base}' does not belong to the chord")
+
+            pitches = pitches[inv_index:] + pitches[:inv_index]
+
+        octave = base_note.octave
+        # If inversion is used, the new base note gets the same octave as the original
+        notes = [Note(pitches[0], base_note.duration, octave)]
+        for pitch in range(1, len(pitches)):
+            if pitches[pitch].letter < pitches[pitch - 1].letter:
+                octave += 1
+
+            notes.append(Note(pitches[pitch], base_note.duration, octave))
+
+        return Chord(notes)
+
+    def serialize(self):
+        return "/".join([note.serialize(with_octave=True) for note in self.notes])
--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -1,5 +1,7 @@
-from alda import pitch
-from alda.core import Duration, Note, Part, Score
+import pytest
+
+from alda import core, pitch
+from alda.core import Duration, Note, Part, Score, Chord
 from alda.pitch import Accidental, Letter, Pitch
 
 
@@ -28,3 +30,68 @@ class TestCore:
     def test_note_duration(self) -> None:
         note = Note(pitch.E, duration=Duration(2))
         assert note.serialize() == "e2"
+
+    def test_chord(self) -> None:
+        note = Note(pitch.C)
+
+        assert Chord.build_chord(note, core.DOM_7) == Chord(
+            [Note(pitch.C), Note(pitch.E), Note(pitch.G), Note(pitch.B_FLAT)]
+        )
+
+    def test_chord_inversion(self) -> None:
+        note = Note(pitch.C)
+
+        assert Chord.build_chord(note, core.DOM_7, pitch.C) == Chord(
+            [Note(pitch.C), Note(pitch.E), Note(pitch.G), Note(pitch.B_FLAT)]
+        )
+        assert Chord.build_chord(note, core.DOM_7, pitch.E) == Chord(
+            [Note(pitch.E), Note(pitch.G), Note(pitch.B_FLAT), Note(pitch.C, octave=4)]
+        )
+        assert Chord.build_chord(note, core.DOM_7, pitch.G) == Chord(
+            [Note(pitch.G), Note(pitch.B_FLAT), Note(pitch.C, octave=4), Note(pitch.E, octave=4)]
+        )
+        assert Chord.build_chord(note, core.DOM_7, pitch.B_FLAT) == Chord(
+            [Note(pitch.B_FLAT), Note(pitch.C, octave=4), Note(pitch.E, octave=4), Note(pitch.G, octave=4)]
+        )
+
+        with pytest.raises(ValueError):
+            Chord.build_chord(note, core.DOM_7, pitch.A)
+
+    def test_all_c_chords(self):
+        note = Note(pitch.C)
+
+        assert Chord.build_chord(note, core.DOM_7, pitch.C) == Chord(
+            [Note(pitch.C), Note(pitch.E), Note(pitch.G), Note(pitch.B_FLAT)]
+        )
+
+        assert Chord.build_chord(note, core.MAJ_7, pitch.C) == Chord(
+            [Note(pitch.C), Note(pitch.E), Note(pitch.G), Note(pitch.B)]
+        )
+
+        assert Chord.build_chord(note, core.MIN_MAJ_7, pitch.C) == Chord(
+            [Note(pitch.C), Note(pitch.E_FLAT), Note(pitch.G), Note(pitch.B)]
+        )
+
+        assert Chord.build_chord(note, core.MIN_7, pitch.C) == Chord(
+            [Note(pitch.C), Note(pitch.E_FLAT), Note(pitch.G), Note(pitch.B_FLAT)]
+        )
+
+        assert Chord.build_chord(note, core.AUG_MAJ_7, pitch.C) == Chord(
+            [Note(pitch.C), Note(pitch.E), Note(pitch.G_SHARP), Note(pitch.B)]
+        )
+
+        assert Chord.build_chord(note, core.AUG_7, pitch.C) == Chord(
+            [Note(pitch.C), Note(pitch.E), Note(pitch.G_SHARP), Note(pitch.B_FLAT)]
+        )
+
+        assert Chord.build_chord(note, core.MIN_7_DIM_5, pitch.C) == Chord(
+            [Note(pitch.C), Note(pitch.E_FLAT), Note(pitch.G_FLAT), Note(pitch.B_FLAT)]
+        )
+
+        assert Chord.build_chord(note, core.DIM_7, pitch.C) == Chord(
+            [Note(pitch.C), Note(pitch.E_FLAT), Note(pitch.G_FLAT), Note(Pitch(Letter.B, [Accidental.FLAT] * 2))]
+        )
+
+        assert Chord.build_chord(note, core.DOM_7_DIM_5, pitch.C) == Chord(
+            [Note(pitch.C), Note(pitch.E), Note(pitch.G_FLAT), Note(pitch.B_FLAT)]
+        )
